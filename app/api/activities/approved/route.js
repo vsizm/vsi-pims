edc8003e8cad4d1d3d38645a951d301b4c11203c
vsizm <vsiprojectszm@gmail.com>
2026-08-29 14:49:21 +0200

    fix: restore approved activity search endpoint

---
+++ app/api/activities/approved/route.js
@@ -0,0 +1,7 @@
+import { GET as getApprovedActivities } from '../../../approved-activities/route';
+
+export async function GET(request) {
+  const response = await getApprovedActivities(request);
+  const activities = await response.json();
+  return Response.json({ activities });
+}
