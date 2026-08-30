17af15f85c6c4fad8f50a5e8a95dec7beac1c890
vsizm <vsiprojectszm@gmail.com>
2026-08-30 16:04:26 +0200

    feat: activate phase 1 executive intelligence dashboard

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,5 +1,5 @@
-import AdminDashboard from './AdminDashboard';
+import Phase1Dashboard from './Phase1Dashboard';
 
 export default function AdminPage() {
-  return <AdminDashboard />;
+  return <Phase1Dashboard />;
 }
