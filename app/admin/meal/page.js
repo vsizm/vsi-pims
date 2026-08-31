76ab7898b4e6741e189b13e0fc1d573e89a9f16a
vsizm <vsiprojectszm@gmail.com>
2026-08-31 16:38:06 +0200

    feat: replace results block with follow-up actions

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -4,6 +4,7 @@ import MealLayoutCleanup from './MealLayoutCleanup';
 import MealDemographicBar from './MealDemographicBar';
 import MealProgrammePerformance from './MealProgrammePerformance';
 import MealReportingCompliance from './MealReportingCompliance';
+import MealFollowupActions from './MealFollowupActions';
 import styles from './meal-overrides.module.css';
 import './meal-activity-layout-fixes.module.css';
 import './meal-hide-activity.css';
@@ -11,7 +12,8 @@ import './meal-donut-fix.module.css';
 import './meal-demographic-bar.css';
 import './meal-programme-performance.css';
 import './meal-reporting-compliance.css';
+import './meal-followup-actions.css';
 
 export default function MealPage() {
-  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><MealProgrammePerformance /><MealReportingCompliance /><Phase4Dashboard /></div>;
+  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><MealProgrammePerformance /><MealReportingCompliance /><MealFollowupActions /><Phase4Dashboard /></div>;
 }
