700524257fcc32b31d6b4b0c19ac15def1eacac1
vsizm <vsiprojectszm@gmail.com>
2026-08-31 14:15:16 +0200

    feat: mount live MEAL demographic bar chart

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -1,11 +1,13 @@
 import Phase4Dashboard from './Phase4Dashboard';
 import MealKpiEnhancer from './MealKpiEnhancer';
 import MealLayoutCleanup from './MealLayoutCleanup';
+import MealDemographicBar from './MealDemographicBar';
 import styles from './meal-overrides.module.css';
 import './meal-activity-layout-fixes.module.css';
 import './meal-hide-activity.css';
 import './meal-donut-fix.module.css';
+import './meal-demographic-bar.css';
 
 export default function MealPage() {
-  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><Phase4Dashboard /></div>;
+  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><Phase4Dashboard /></div>;
 }
