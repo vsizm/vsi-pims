f47a2c8c3cc6a71010d91b3d01c103ca936915a1
vsizm <vsiprojectszm@gmail.com>
2026-08-31 15:23:02 +0200

    feat: mount clickable MEAL programme performance activity view

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -2,12 +2,14 @@ import Phase4Dashboard from './Phase4Dashboard';
 import MealKpiEnhancer from './MealKpiEnhancer';
 import MealLayoutCleanup from './MealLayoutCleanup';
 import MealDemographicBar from './MealDemographicBar';
+import MealProgrammePerformance from './MealProgrammePerformance';
 import styles from './meal-overrides.module.css';
 import './meal-activity-layout-fixes.module.css';
 import './meal-hide-activity.css';
 import './meal-donut-fix.module.css';
 import './meal-demographic-bar.css';
+import './meal-programme-performance.css';
 
 export default function MealPage() {
-  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><Phase4Dashboard /></div>;
+  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><MealProgrammePerformance /><Phase4Dashboard /></div>;
 }
