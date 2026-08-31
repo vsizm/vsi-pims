f8b8cdd6df5a5a42ba5f8c1bafe005056973a516
vsizm <vsiprojectszm@gmail.com>
2026-08-31 12:15:07 +0200

    feat: mount MEAL directorate and location KPI cards

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -1,6 +1,7 @@
 import Phase4Dashboard from './Phase4Dashboard';
+import MealKpiEnhancer from './MealKpiEnhancer';
 import styles from './meal-overrides.module.css';
 
 export default function MealPage() {
-  return <div className={styles.scope}><Phase4Dashboard /></div>;
+  return <div className={styles.scope}><MealKpiEnhancer /><Phase4Dashboard /></div>;
 }
