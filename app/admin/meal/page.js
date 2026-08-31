abe9b080b32a007df0626de5db4280d38adf24a6
vsizm <vsiprojectszm@gmail.com>
2026-08-31 13:41:11 +0200

    fix: hide redundant MEAL activity details block

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -3,6 +3,7 @@ import MealKpiEnhancer from './MealKpiEnhancer';
 import MealLayoutCleanup from './MealLayoutCleanup';
 import styles from './meal-overrides.module.css';
 import './meal-activity-layout-fixes.module.css';
+import './meal-hide-activity.css';
 
 export default function MealPage() {
   return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><Phase4Dashboard /></div>;
