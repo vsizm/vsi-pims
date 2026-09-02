ec7d2f41161894aa1be1923f0c3944de5c5902d0
vsizm <vsiprojectszm@gmail.com>
2026-09-02 10:56:07 +0200

    remove: take MEAL follow-up actions off intelligence page

--- app/admin/meal/page.js
+++ app/admin/meal/page.js
@@ -4,7 +4,6 @@ import MealLayoutCleanup from './MealLayoutCleanup';
 import MealDemographicBar from './MealDemographicBar';
 import MealProgrammePerformance from './MealProgrammePerformance';
 import MealReportingCompliance from './MealReportingCompliance';
-import MealFollowupActions from './MealFollowupActions';
 import styles from './meal-overrides.module.css';
 import './meal-section-header-theme.css';
 import './meal-activity-layout-fixes.module.css';
@@ -17,5 +16,5 @@ import './meal-followup-actions.css';
 import './meal-section-spacing.css';
 
 export default function MealPage() {
-  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><MealProgrammePerformance /><MealReportingCompliance /><MealFollowupActions /><Phase4Dashboard /></div>;
+  return <div className={styles.scope}><MealLayoutCleanup /><MealKpiEnhancer /><MealDemographicBar /><MealProgrammePerformance /><MealReportingCompliance /><Phase4Dashboard /></div>;
 }
